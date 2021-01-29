7e368c34c3f06652e62191a918735d573c8b33a0
LHY <847798512@qq.com>
2021-01-29 14:55:59 +0800

    Work.vue increase website

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,5 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
  git push -f git@github.com:JamesLHY/myBlog.io.git master
 
+
 cd -
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -5,107 +5,143 @@
             <div class="work">
                 <div class="work-title"><p>Code</p></div>
                 <ul>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-vue-notepad"></use>
-                            </svg>
-                            <figcaption>小白记账Vue版</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-react-notepad"></use>
-                            </svg>
-                            <figcaption>小白记账React版</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-vue3"></use>
-                            </svg>
-                            <figcaption>Vue3-UI框架</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-translate"></use>
-                            </svg>
-                            <figcaption>Node翻译</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-nav-website"></use>
-                            </svg>
-                            <figcaption>老早的导航网站</figcaption>
-                        </figure>
-                    </li>
+                    <a href="https://jameslhy.github.io/xiaobai-notepad-vue-version--website/" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-vue-notepad"></use>
+                                </svg>
+                                <figcaption>小白记账Vue版</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/xiaobai-notePad-react-version-website/" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-react-notepad"></use>
+                                </svg>
+                                <figcaption>小白记账React版</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/Vue3-ui-website/" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-vue3"></use>
+                                </svg>
+                                <figcaption>Vue3-Pure-UI</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://github.com/JamesLHY/node-fanyi" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-translate"></use>
+                                </svg>
+                                <figcaption>Node翻译</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/myNav/src/index.html" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-nav-website"></use>
+                                </svg>
+                                <figcaption>老早的导航网站</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+
                 </ul>
             </div>
             <div class="work">
                 <div class="work-title"><p>Gadget</p></div>
                 <ul>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-canvas"></use>
-                            </svg>
-                            <figcaption>Canvas</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-pikachu"></use>
-                            </svg>
-                            <figcaption>Pikachu</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-yinyang"></use>
-                            </svg>
-                            <figcaption>阴阳八卦</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-clock"></use>
-                            </svg>
-                            <figcaption>大时钟</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-charge"></use>
-                            </svg>
-                            <figcaption>Charge</figcaption>
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <svg class="icon" aria-hidden="true">
-                                <use xlink:href="#icon-cyberpunk"></use>
-                            </svg>
-                            <figcaption>Cyberpunk Button</figcaption>
-                        </figure>
-                    </li>
+                    <a href="https://jameslhy.github.io/canvas/" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-canvas"></use>
+                                </svg>
+                                <figcaption>Canvas</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/Pikachu-1/src/index.html" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-pikachu"></use>
+                                </svg>
+                                <figcaption>Pikachu</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/yin-yang/src/index.html" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-yinyang"></use>
+                                </svg>
+                                <figcaption>阴阳八卦</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/a-clock/" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-clock"></use>
+                                </svg>
+                                <figcaption>大时钟</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/charge-demo/" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-charge"></use>
+                                </svg>
+                                <figcaption>Charge</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/cyberpunk-button/" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-cyberpunk"></use>
+                                </svg>
+                                <figcaption>Cyberpunk Button</figcaption>
+                            </figure>
+                        </li>
+                    </a>
+                    <a href="https://jameslhy.github.io/404page/src/index.html" target="_blank">
+                        <li>
+                            <figure>
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-404"></use>
+                                </svg>
+                                <figcaption>404</figcaption>
+                            </figure>
+                        </li>
+                    </a>
                 </ul>
             </div>
-            <div class="view-more-wrapper">
-                <svg class="icon" aria-hidden="true">
-                    <use xlink:href="#icon-view-more"></use>
-                </svg>
-                <span class="view-more-text">VIEW MORE</span>
-            </div>
+            <a href="https://github.com/JamesLHY" target="_Blank">
+                <div class="view-more-wrapper">
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-view-more"></use>
+                    </svg>
+                    <span class="view-more-text">VIEW MORE</span>
+                </div>
+            </a>
+
         </div>
     </div>
 </template>
@@ -120,19 +156,21 @@
 </script>
 
 <style scoped lang="scss">
-    .view-more-wrapper{
+    .view-more-wrapper {
         display: flex;
         align-items: center;
         cursor: pointer;
         opacity: .5;
-        > .view-more-text{
+
+        > .view-more-text {
             padding-left: 10px;
-            color: hsla(0,0%,100%,.6);
+            color: hsla(0, 0%, 100%, .6);
             font-size: 20px;
             font-weight: bold;
             line-height: 46px;
         }
-        &:hover{
+
+        &:hover {
             opacity: 1;
         }
     }
@@ -178,33 +216,34 @@
                 }
 
                 > ul {
-                    width: 80vw;
-
                     display: flex;
                     flex-wrap: wrap;
                     margin-top: 26px;
 
-                    > li {
-                        margin-right: 70px;
+                    > a {
+                        > li {
+                            margin-right: 70px;
 
-                        > figure {
-                            display: flex;
-                            flex-direction: column;
-                            justify-content: center;
-                            align-items: center;
-                            margin: 12px;
-                            max-width: 88px;
-                            cursor: pointer;
+                            > figure {
+                                display: flex;
+                                flex-direction: column;
+                                justify-content: center;
+                                align-items: center;
+                                margin: 12px;
+                                max-width: 88px;
+                                cursor: pointer;
 
-                            > figcaption {
-                                text-align: center;
-                                color: #fff;
-                                font-size: 20px;
-                                margin-top: 10px;
-                                transition: transform .3s ease-out
+                                > figcaption {
+                                    text-align: center;
+                                    color: #fff;
+                                    font-size: 20px;
+                                    margin-top: 10px;
+                                    transition: transform .3s ease-out
+                                }
                             }
                         }
                     }
+
                 }
             }
 
